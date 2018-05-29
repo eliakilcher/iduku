a5b8449d5b792ae124503f06dec0b670eeea8dd7
eliakilcher <eliakilcher@users.noreply.github.com>
2018-05-29 14:15:40 +0200

    parallax

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@ $('a.burger').click(function(event) {
      $('ul.navi').toggleClass('nav-block');
 });
 
-
+$(window).scroll(function(e){
+	parallax();
+});
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.slider').css('top', +(scrolled * 0.5) + 'px');
+}
 
 
 
